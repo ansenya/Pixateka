3223cc3049e477d3a115956a48c27f0901cef25a
ansenya <ars@made.ru>
2023-02-18 16:03:13 +0300

    AKA pinterest

---
+++ app/src/main/java/ru/senya/pixateka/adapters/RecyclerTouchListener.java
@@ -0,0 +1,60 @@
+package ru.senya.pixateka.adapters;
+
+
+import android.view.MotionEvent;
+import android.content.Context;
+import android.view.GestureDetector;
+import android.view.View;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerTouchListener implements RecyclerView.OnItemTouchListener{
+    private final GestureDetector gestureDetector;
+    private final ClickListener clickListener;
+    public RecyclerTouchListener(Context context, final RecyclerView recyclerView, final ClickListener clickListener) {
+        this.clickListener = clickListener;
+        gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+
+                return true;
+
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+
+                View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+
+                if (child != null && clickListener != null) {
+
+                    clickListener.onLongClick(child, recyclerView.getChildAdapterPosition(child));
+
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+        View child = rv.findChildViewUnder(e.getX(), e.getY());
+        if (child != null && clickListener != null && gestureDetector.onTouchEvent(e)) {
+            clickListener.onClick(child, rv.getChildAdapterPosition(child));
+        }
+        return false;
+    }
+
+    @Override
+    public void onTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
+    }
+
+    @Override
+    public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+    }
+
+
+    public interface ClickListener {
+        void onClick(View view, int position);
+        void onLongClick(View view, int position);
+    }
+}
